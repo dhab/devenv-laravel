4e3f48a919930338e63f43fcdb8b66213818dcd1
Dennis Holmen <dennis.holmen@dreamhack.se>
2017-03-22 14:05:42 +0100

    Changes for debian/fpm image and requirements.

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
-FROM php:alpine
-MAINTAINER hitalos <hitalos@gmail.com>
+FROM php:fpm
+MAINTAINER Dennis Holmen <dennis.holmen@gmail.com>
 
-RUN apk update && apk upgrade && apk add git
+RUN apt-get update 
+RUN apt-get upgrade 
+RUN apt-get openssh-client install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev libwebp-dev openldap-dev postgresql-dev jpeg-dev icu-dev gettext-dev freetype-dev freetds-dev freetds freetype icu libintl libldap libjpeg libmcrypt libpng libpg libwebp
 
 # Install PHP extensions
 ADD install-php.sh /usr/sbin/install-php.sh
 RUN /usr/sbin/install-php.sh
-
-# Download and install NodeJS
-ENV NODE_VERSION 7.7.2
-ADD install-node.sh /usr/sbin/install-node.sh
-RUN /usr/sbin/install-node.sh
-RUN npm i -g yarn
 
 WORKDIR /var/www
 CMD php ./artisan serve --port=80 --host=0.0.0.0
--- install-php.sh
+++ install-php.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-apk add freetds freetype icu libintl libldap libjpeg libmcrypt libpng libpq libwebp
-
-TMP="curl-dev \
-		freetds-dev \
-    freetype-dev \
-    gettext-dev \
-    icu-dev \
-    jpeg-dev \
-    libmcrypt-dev \
-    libpng-dev \
-    libwebp-dev \
-    libxml2-dev \
-    openldap-dev \
-    postgresql-dev"
-apk add $TMP
-
 # Configure extensions
 docker-php-ext-configure gd --with-jpeg-dir=usr/ --with-freetype-dir=usr/ --with-webp-dir=usr/
 docker-php-ext-configure ldap --with-libdir=lib/
@@ -28,19 +12,33 @@ docker-php-ext-configure pdo_dblib --with-libdir=lib/
     mv /usr/src/php/ext/mongo* /usr/src/php/ext/mongodb
 
 docker-php-ext-install \
+    bcmath \
+    bz2 \
     curl \
     exif \
     gd \
     gettext \
     intl \
+    json \
     ldap \
+    mbstring \
     mcrypt \
     mongodb \
+    opcache \
     pdo_dblib \
     pdo_mysql \
     pdo_pgsql \
+    xml \
     xmlrpc \
     zip
+
+# Download trusted certs 
+mkdir -p /etc/ssl/certs && update-ca-certificates
+
+# Install composer
+php -r "readfile('https://getcomposer.org/installer');" | php && \
+   mv composer.phar /usr/bin/composer && \
+   chmod +x /usr/bin/composer
 
 # Download trusted certs 
 mkdir -p /etc/ssl/certs && update-ca-certificates
